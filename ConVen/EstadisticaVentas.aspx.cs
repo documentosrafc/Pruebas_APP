3db750fecd91e3594613cc7104c77133826ae2f7
documentosrafc <documentosrafc@gmail.com>
2019-01-29 14:05:26 -0400

    Create EstadisticaVentas.aspx.cs

---
+++ ConVen/EstadisticaVentas.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+using Microsoft.Reporting.Common;
+
+public partial class EstadisticaVentas : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["NombreCompleto"] == null)
+        {
+            Response.Redirect("~/Default.aspx");
+        }
+        if (!IsPostBack)
+        {
+            habilitarmenu();
+            this.Form.Attributes.Add("autocomplete", "off");
+
+            ReportParameter[] parametr = new ReportParameter[1];
+            string GRUPO = Session["NumeroGrupo"].ToString();
+            parametr[0] = new ReportParameter("GRUPO", GRUPO);            
+            this.ReportViewer1.ServerReport.SetParameters(parametr);
+        }
+    }
+    private void habilitarmenu()
+    {
+        var Menu = Page.Master.FindControl("Menu1") as Menu;
+        if (Session["JEFAZONA"].ToString() == "1")
+        {
+            Menu.Visible = true;
+        }
+    }
+}
