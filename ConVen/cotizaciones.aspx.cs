371503ad39c32f7957dde4cdf7dcc55699863e51
documentosrafc <documentosrafc@gmail.com>
2019-01-29 14:01:02 -0400

    Create cotizaciones.aspx.cs

---
+++ ConVen/cotizaciones.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.Reporting.WebForms;
+using Microsoft.Reporting.Common;
+
+public partial class cotizaciones : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["NombreCompleto"] == null)
+        {
+            Response.Redirect("~/Default.aspx");
+        }
+        if (!IsPostBack)
+        {
+            this.Form.Attributes.Add("autocomplete", "off");
+ 
+            ReportParameter[] parametr = new ReportParameter[1];
+            string cotiza = Session["numcotiza"].ToString();
+            parametr[0] = new ReportParameter("COTIZACIONNUM", cotiza);            
+            this.ReportViewer1.ServerReport.SetParameters(parametr);
+        }
+    }
+}
